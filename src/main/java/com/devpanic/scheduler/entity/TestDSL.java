52903e776c902d5aa0db64c6acd8d7e53bdeb9ff
devpanic <yeon.dev.8@gmail.com>
2024-09-18 21:41:28 +0900

    feat: Query DSL 적용을 위한 build.gradle, gitignore, testDSL 엔티티 선언

---
+++ src/main/java/com/devpanic/scheduler/entity/TestDSL.java
@@ -0,0 +1,53 @@
+package com.devpanic.scheduler.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class TestDSL {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public TestDSL() {
+    }
+
+    public TestDSL(Long id, String title, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
